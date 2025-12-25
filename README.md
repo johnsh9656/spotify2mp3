2d39914c67aefd16902d5b02418c2efbe24917ff
johnsh9656 <harrisonjohns1219@gmail.com>
2025-12-25 18:25:58 -0500

    initial setup, create requirements.txt and spotify_download.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spotify2mp3
+
+### Setup
+pip install -r requirements.txt
+spotdl --install-ffmpeg
+venv/Scripts/activate
+
+
+### Project
+#### spotify_download.py
+download_spotify_track: downloads mp3 of given spotify track link to specified path
+download_spotify_playlist: downloads mp3s of given spotify playlist link to specified path
+download_spotify_album: downloads mp3s of given spotify album link to specified path
---
+++ spotify_download.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def download_spotify_track(spotify_url: str, output_path: str) -> None:
+    """
+    Docstring for download_spotify_track
+    
+    :param spotify_url: Description
+    :type spotify_url: str
+    :param output_path: Description
+    :type output_path: str
+    """
+
+def download_spotify_playlist(spotify_url: str, outputh_path: str) -> None:
+    """
+    Docstring for download_spotify_playlist
+    
+    :param spotify_url: Description
+    :type spotify_url: str
+    :param outputh_path: Description
+    :type outputh_path: str
+    """
+
+def download_spotify_album(spotify_url: str, output_path: str) -> None:
+    """
+    Docstring for download_spotify_album
+    
+    :param spotify_url: Description
+    :type spotify_url: str
+    :param output_path: Description
+    :type output_path: str
+    """
